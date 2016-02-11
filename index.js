ea2b239b658199d6f563e23a3173e73307becf4c
SeokJun Hong <hong921122@gmail.com>
2016-02-11 22:38:06 +0900

    add argument

--- index.js
+++ index.js
@@ -9,15 +9,23 @@ program
 	.option('-f, --file [value]', 'Add file path')
 	.option('-i, --interval <n>', 'Set interval')
 	.option('-r, --regex [value]', 'Set regex')
+	.option('-d, --delim [value]', 'Set delimeter')
 	.parse(process.argv);
 
 var filename = program.file;
 var interval = program.interval || 1000;
 var regex = program.regex || "";
+var delim = program.delim || "\n";
+
+if (!filename) {
+	console.error("no select file");
+	program.outputHelp();
+	process.exit(1);
+}
 
 if (!fs.existsSync(filename)) fs.writeFileSync(filename, "");
 
-var tail = new Tail(filename, '\n', {interval:1000});
+var tail = new Tail(filename, delim, {interval:interval});
 
 tail.on('line', function(data) {
 	var tail_reg = new RegExp(regex, "g");
@@ -26,8 +34,6 @@ tail.on('line', function(data) {
 	if((result = tail_reg.exec(data)) != null) {
 		var colored_string = data.substring(result.index, tail_reg.lastIndex);
 
-		console.log(result);
-		console.log(tail_reg.lastIndex);
 		console.log(
 			data.substring(0, result.index) + 
 			clc.red(colored_string) + 
